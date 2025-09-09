de9762772822510ce1cba45621acf45f00995a72
DerekMatos-1 <derek.matos1@upr.edu>
2025-09-09 14:51:57 +0000

    se le anadio la forma para poder ponerolos numero de mayor a menor

--- main.cpp
+++ main.cpp
@@ -14,7 +14,35 @@ int main(){
     int numA2 = rand() % (100 - 0 + 1) + 0;
     int numA3 = rand() % (100 - 0 + 1) + 0;
 
-    cout << numA1 << " "<< numA2 << " "<< numA3 << endl;
+    cout << "numero aeatorio generador:" << numA1 << " "<< numA2 << " "<< numA3 << endl;
+
+    if( numA1 >= numA2 && numA1 >= numA3){
+        if (numA2 >= numA3){
+            cout << "orden desendiente:" << numA1 << " " << numA2 << " " << numA3 << endl;
+        }
+        else {
+            cout << "orden desendiente:" << numA1 << " " << numA3 << " " << numA2 << endl;
+        }
+    }
+    else if (numA2 >= numA1 && numA2 >= numA3){
+        if (numA1 >= numA3){
+            cout << "orden desendiente:" << numA2 << " " << numA1 << " " << numA3 << endl;
+        }
+        else {
+            cout << "orden desendiente:" << numA2 << " " << numA3 << " " << numA1<< endl;
+        }
+    }
+    else if (numA3 >= numA1 && numA3 >= numA2){
+        if (numA1 >= numA2){
+            cout << "orden desendiente:" << numA3 << " " << numA1 << " " << numA2 << endl;
+        }
+        else {
+            cout "orden desendiente:" << numA3 << " " << numA2 << " " << numA1 << endl;
+        }
+    }
+    else {
+        cout << "No es posible pornelo en forma desendiente."
+    }
 
     return 0;
 
