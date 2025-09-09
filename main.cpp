9240057008ca941d74fa1ceb02f29db29565e803
DerekMatos-1 <derek.matos1@upr.edu>
2025-09-09 14:56:46 +0000

    se anadio >=

--- main.cpp
+++ main.cpp
@@ -14,30 +14,30 @@ int main(){
     int numA2 = rand() % (100 - 0 + 1) + 0;
     int numA3 = rand() % (100 - 0 + 1) + 0;
 
-    cout << "numero aeatorio generador:" << numA1 << " "<< numA2 << " "<< numA3 << endl;
+    cout << "numero aeatorio generador:" << numA1 << " >= "<< numA2 << " >= "<< numA3 << endl;
 
     if( numA1 >= numA2 && numA1 >= numA3){
         if (numA2 >= numA3){
-            cout << "orden desendiente:" << numA1 << " " << numA2 << " " << numA3 << endl;
+            cout << "orden desendiente:" << numA1 << " >= " << numA2 << " >= " << numA3 << endl;
         }
         else {
-            cout << "orden desendiente:" << numA1 << " " << numA3 << " " << numA2 << endl;
+            cout << "orden desendiente:" << numA1 << " >= " << numA3 << " >= " << numA2 << endl;
         }
     }
     else if (numA2 >= numA1 && numA2 >= numA3){
         if (numA1 >= numA3){
-            cout << "orden desendiente:" << numA2 << " " << numA1 << " " << numA3 << endl;
+            cout << "orden desendiente:" << numA2 << " >= " << numA1 << " >= " << numA3 << endl;
         }
         else {
-            cout << "orden desendiente:" << numA2 << " " << numA3 << " " << numA1<< endl;
+            cout << "orden desendiente:" << numA2 << " >= " << numA3 << " >= " << numA1<< endl;
         }
     }
     else if (numA3 >= numA1 && numA3 >= numA2){
         if (numA1 >= numA2){
-            cout << "orden desendiente:" << numA3 << " " << numA1 << " " << numA2 << endl;
+            cout << "orden desendiente:" << numA3 << " >= " << numA1 << " >= " << numA2 << endl;
         }
         else {
-            cout << "orden desendiente:" << numA3 << " " << numA2 << " " << numA1 << endl;
+            cout << "orden desendiente:" << numA3 << " >= " << numA2 << " >= " << numA1 << endl;
         }
     }
     else {
