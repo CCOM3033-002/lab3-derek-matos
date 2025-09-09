fc8a32fc1158d4481c02e51633a768c1d6890f16
DerekMatos-1 <derek.matos1@upr.edu>
2025-09-09 15:01:29 +0000

    se modifico orden descendiente para que se vea mejor

--- main.cpp
+++ main.cpp
@@ -15,33 +15,34 @@ int main(){
     int numA3 = rand() % (100 - 0 + 1) + 0;
 
     cout << "numero aeatorio generador:" << numA1 << " "<< numA2 << " "<< numA3 << endl;
+    cout << "orden descendiente: ";
 
     if( numA1 >= numA2 && numA1 >= numA3){
         if (numA2 >= numA3){
-            cout << "orden desendiente:" << numA1 << " >= " << numA2 << " >= " << numA3 << endl;
+            cout << numA1 << " >= " << numA2 << " >= " << numA3 << endl;
         }
         else {
-            cout << "orden desendiente:" << numA1 << " >= " << numA3 << " >= " << numA2 << endl;
+            cout << numA1 << " >= " << numA3 << " >= " << numA2 << endl;
         }
     }
     else if (numA2 >= numA1 && numA2 >= numA3){
         if (numA1 >= numA3){
-            cout << "orden desendiente:" << numA2 << " >= " << numA1 << " >= " << numA3 << endl;
+            cout << numA2 << " >= " << numA1 << " >= " << numA3 << endl;
         }
         else {
-            cout << "orden desendiente:" << numA2 << " >= " << numA3 << " >= " << numA1<< endl;
+            cout << numA2 << " >= " << numA3 << " >= " << numA1<< endl;
         }
     }
     else if (numA3 >= numA1 && numA3 >= numA2){
         if (numA1 >= numA2){
-            cout << "orden desendiente:" << numA3 << " >= " << numA1 << " >= " << numA2 << endl;
+            cout << numA3 << " >= " << numA1 << " >= " << numA2 << endl;
         }
         else {
-            cout << "orden desendiente:" << numA3 << " >= " << numA2 << " >= " << numA1 << endl;
+            cout << numA3 << " >= " << numA2 << " >= " << numA1 << endl;
         }
     }
     else {
-        cout << "No es posible pornelo en forma desendiente." << endl;
+        cout << "No es posible pornelo en forma descendiente." << endl;
     }
 
     return 0;
