c6e7ced685e8d46ce0857b0837c79ab177ce02ec
DerekMatos-1 <derek.matos1@upr.edu>
2025-09-09 14:31:24 +0000

    se le anadioo los otros numeros

--- main.cpp
+++ main.cpp
@@ -10,9 +10,11 @@ int main(){
 
     srand(time(0));
 
-    int numeroAleatorio = rand() % (100 - 0 + 1) + 0;
+    int numA1 = rand() % (100 - 0 + 1) + 0;
+    int numA2 = rand() % (100 - 0 + 1) + 0;
+    int numA3 = rand() % (100 - 0 + 1) + 0;
 
-    cout << numeroAleatorio << endl;
+    cout << numeroA1 << " "<< numA2 << " "<< numA3 << endl;
 
     return 0;
 
