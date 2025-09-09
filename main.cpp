d7c2e968f0133180f14fe40704949f2cae86dd1b
DerekMatos-1 <derek.matos1@upr.edu>
2025-09-09 14:26:26 +0000

    se le anadio srad y se va a tratar si el numero aleatorio funciona

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+/*Derek Matos 
+septiembre 9 de 2025
+Lab #3: Numeros ALeatorios*/
 
+#include<iostream>
+#include<cstdlib>
+Using namespace std;
+
+int main(){
+
+    srand(time(0));
+
+    int numeroAleatorio = rand() % (100 - 0 + 1) + 0;
+
+}
